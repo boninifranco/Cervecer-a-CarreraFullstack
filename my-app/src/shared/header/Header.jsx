7308b7117a0b282b9d6457d7b9c7bcc465367c8a
Agus-Silva-CPFS <agussilva23.work@gmail.com>
2023-12-06 20:08:10 -0300

    Se cambio el nombre de una clase

--- my-app/src/shared/header/Header.jsx
+++ my-app/src/shared/header/Header.jsx
@@ -5,7 +5,7 @@ export const Header = () => {
   return (
     <div className="Header">
     <nav className="navPersonalizado navbar navbar-expand-lg navbar-dark color-nav">
-    <div className="container-fluid agu">
+    <div className="container-fluid nav-superior">
       <div className='logoNav'></div>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
