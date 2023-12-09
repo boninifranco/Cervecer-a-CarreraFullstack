d8995756aa29b772bde9276f33ca0f0fd3b5bb5f
TG-98 <tobiasganin@hotmail.com>
2023-12-09 19:55:49 -0300

    Se agregaron imagenes al carrusel principal

--- my-app/src/components/Nosotros/Horarios.jsx
+++ my-app/src/components/Nosotros/Horarios.jsx
@@ -7,7 +7,7 @@ function Horarios() {
   return (
     <div className='ubicacion-horarios'>
         <img className='logo-ubicacion' width="200" height="200" src={logoDireccion} alt="" />
-        <p><span className='color-primario-nosotros'>Klein Venedig 1, 78462 Konstanz, Alemania</span></p>
+        <p><span className='color-primario-nosotros'>Bolívar 1701, C1141 CABA</span></p>
         <img className='logo-hora' width='200' height='200' src={logoHora} alt="" />
         <p><span className='color-primario-nosotros'>Lunes a viernes 19:30hs Sabado y domingo 18:00hs</span></p>
     </div>
---
+++ my-app/src/components/home/Videoilustrativo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Videoilustrativo() {
+  return (
+    <div className='video'>
+        <iframe className='videoyt' style={{width: "100%", height: "384px"}} src="https://www.youtube.com/embed/UfDZ8KDXKqg?si=QPFk7VZAWXeeqbvb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+  )
+}
+
+export default Videoilustrativo
