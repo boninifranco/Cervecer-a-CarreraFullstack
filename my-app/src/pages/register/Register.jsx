fe483eaebcf284703ba2ade4b44b98be86c18c9a
PedroSa133 <pedrosanchez_3@hotmail.com>
2023-12-10 00:46:19 -0300

    Se agrega la page register

---
+++ my-app/src/pages/register/Register.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './register.css';
+
+import { FormRegister } from '../../components/form-register/FormRegister';
+
+function Register() {
+    return (
+       <React.Fragment>
+        <FormRegister />
+       </React.Fragment>
+    );
+}
+
+export { Register }
